dd0bc939ba4c51e6b319cb7ed1657b9c23a00ac2
Vanessa <nessa1vane@icloud.com>
2022-01-24 18:39:56 -0300

    modal simples com os livros

--- frontend/src/components/book/index.jsx
+++ frontend/src/components/book/index.jsx
@@ -1,14 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import { Container, Button, Typography, Modal, Fade } from '@mui/material';
 
 // local
 import styles from './styles';
 
 const Book = ({ book }) => {
+
+    const [open, setOpen] = useState(false);
+
+    function handleOpen() {
+        setOpen(true);
+    }
+
+    function handleClose() {
+        setOpen(false);
+    }
+
     return (
         <>
-            <img className={styles().thumb} src={book.image} alt={book.title}/>
+            <img className={styles().thumb} src={book.image} alt={book.title} onClick={handleOpen} />
+
+            <Modal className={styles().modal} aria-labelledby="Confirmar" aria-describedby="Confirmar senha" closeAfterTransition
+                open={open} onClose={handleClose} >
+                <Fade in={open}>
+                    <div className={styles().windowModal} style={{ textAlign: 'center' }}>
+                        <img className={styles().thumb} src={book.image} alt={book.title} onClick={handleOpen} />
+                        <Typography variant="h6" component="h2">
+                            {book.title}
+                        </Typography>
+                        <Typography>
+                            {book.description}
+                        </Typography>
+                    </div>
+                </Fade>
+            </Modal>
         </>
     );
 }
- 
+
 export default Book;
--- frontend/src/components/book/styles.js
+++ frontend/src/components/book/styles.js
@@ -3,8 +3,27 @@ import { makeStyles } from '@material-ui/styles';
 const styles = makeStyles({
 
     thumb: {
-        width: '10%',
-    }
+        height: '28vh'
+    },
+
+    modal: {
+        maxWidth: '100%',
+        maxHeight: '100%',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+    windowModal: {
+        backgroundColor: '#606060',
+        alignItems: 'center',
+        display: 'block',
+        borderRadius: '5px',
+        borderColor:'black',
+        padding: '18px 18px 15px 18px',
+        maxHeight: '100%',
+        maxWidth: '100%',
+    },
 
 });
 

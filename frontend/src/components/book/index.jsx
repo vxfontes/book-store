0de313f3d6d7e69af790f833ca606e90befeddc3
Vanessa <nessa1vane@icloud.com>
2022-01-26 14:05:49 -0300

    configurações de modal

--- frontend/src/components/book/index.jsx
+++ frontend/src/components/book/index.jsx
@@ -1,8 +1,20 @@
 import React, { useState } from "react";
-import { Container, Button, Typography, Modal, Fade } from '@mui/material';
+import { Container, Button, Typography, Modal, Fade, Box } from '@mui/material';
 
 // local
 import styles from './styles';
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
 
 const Book = ({ book }) => {
 
@@ -20,20 +32,40 @@ const Book = ({ book }) => {
         <>
             <img className={styles().thumb} src={book.image} alt={book.title} onClick={handleOpen} />
 
-            <Modal className={styles().modal} aria-labelledby="Confirmar" aria-describedby="Confirmar senha" closeAfterTransition
-                open={open} onClose={handleClose} >
-                <Fade in={open}>
-                    <div className={styles().windowModal} style={{ textAlign: 'center' }}>
-                        <img className={styles().thumb} src={book.image} alt={book.title} onClick={handleOpen} />
-                        <Typography variant="h6" component="h2">
-                            {book.title}
+            {/* <div className={styles().modal} >
+                <Modal closeAfterTransition open={open} onClose={handleClose} >
+                    <Fade in={open}>
+                        <div className={styles().windowModal} style={{ textAlign: 'center' }}>
+                            <img className={styles().thumb} src={book.image} alt={book.title} onClick={handleOpen} />
+                            <Typography variant="h6" component="h2">
+                                {book.title}
+                            </Typography>
+                            <Typography>
+                                {book.description}
+                            </Typography>
+                        </div>
+                    </Fade>
+                </Modal>
+            </div> */}
+
+            <div>
+                <Button onClick={handleOpen}>Open modal</Button>
+                <Modal
+                    open={open}
+                    onClose={handleClose}
+                    aria-labelledby="modal-modal-title"
+                    aria-describedby="modal-modal-description"
+                >
+                    <Box sx={style}>
+                        <Typography id="modal-modal-title" variant="h6" component="h2">
+                            Text in a modal
                         </Typography>
-                        <Typography>
-                            {book.description}
+                        <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
                         </Typography>
-                    </div>
-                </Fade>
-            </Modal>
+                    </Box>
+                </Modal>
+            </div>
         </>
     );
 }
--- frontend/src/components/book/styles.js
+++ frontend/src/components/book/styles.js
@@ -7,22 +7,36 @@ const styles = makeStyles({
     },
 
     modal: {
-        maxWidth: '100%',
-        maxHeight: '100%',
+        maxWidth: '60%',
+        minHeight: '60%',
         display: 'flex',
         alignItems: 'center',
         justifyContent: 'center',
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        border: 'none'
     },
 
     windowModal: {
         backgroundColor: '#606060',
-        alignItems: 'center',
-        display: 'block',
+        // alignItems: 'center',
+        // display: 'block',
         borderRadius: '5px',
-        borderColor:'black',
+        // borderColor: 'black',
         padding: '18px 18px 15px 18px',
-        maxHeight: '100%',
-        maxWidth: '100%',
+        // maxHeight: '100%',
+        // maxWidth: '100%',
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: 1200,
+        height: 500,
+        bgcolor: 'background.paper',
+        border: '2px solid #000',
+        boxShadow: 24,
+        p: 4,
     },
 
 });

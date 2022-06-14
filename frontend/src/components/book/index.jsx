39a3b314969d4c498e7e2acb667d1762269d833a
vxfontes <nessa1vane@icloud.com>
2022-06-14 13:16:53 -0300

    atts de formatação

--- frontend/src/components/book/index.jsx
+++ frontend/src/components/book/index.jsx
@@ -14,7 +14,7 @@ const style = {
     border: '2px solid #000',
     boxShadow: 24,
     p: 4,
-  };
+};
 
 const Book = ({ book }) => {
 
@@ -30,42 +30,19 @@ const Book = ({ book }) => {
 
     return (
         <>
-            <img className={styles().thumb} src={book.image} alt={book.title} onClick={handleOpen} />
-
-            {/* <div className={styles().modal} >
-                <Modal closeAfterTransition open={open} onClose={handleClose} >
-                    <Fade in={open}>
-                        <div className={styles().windowModal} style={{ textAlign: 'center' }}>
-                            <img className={styles().thumb} src={book.image} alt={book.title} onClick={handleOpen} />
-                            <Typography variant="h6" component="h2">
-                                {book.title}
-                            </Typography>
-                            <Typography>
-                                {book.description}
-                            </Typography>
-                        </div>
-                    </Fade>
-                </Modal>
-            </div> */}
-
-            <div>
-                <Button onClick={handleOpen}>Open modal</Button>
-                <Modal
-                    open={open}
-                    onClose={handleClose}
-                    aria-labelledby="modal-modal-title"
-                    aria-describedby="modal-modal-description"
-                >
-                    <Box sx={style}>
-                        <Typography id="modal-modal-title" variant="h6" component="h2">
-                            Text in a modal
-                        </Typography>
-                        <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-                        </Typography>
-                    </Box>
-                </Modal>
-            </div>
+            <img className={styles().thumb} src={book.image} alt={book.title} width="10%" onClick={handleOpen} />
+            
+            <Modal className={styles().modal}
+                open={open} onClose={() => setopen(false)} closeAfterTransition BackdropComponent={Backdrop} BackdropProps={{ timeout: 750 }}>
+                <Fade in={open}>
+                    <div className={styles().windowModal} style={{ textAlign: 'center' }}>
+                        <Typography variant="h4" gutterBottom component="div">Aviso</Typography>
+                        <Typography style={{ marginBottom: '15px' }} variant="body1" gutterBottom component="div">Deseja apagar {subject.name}?</Typography>
+                        <ColorButtonRed style={{ marginRight: '15px' }} variant="contained" color="primary" onClick={() => { deletar(); setopen(false) }}>Sim</ColorButtonRed>
+                        <Button variant="contained" color="secondary" onClick={() => { setopen(false) }}>Não</Button>
+                    </div>
+                </Fade>
+            </Modal>
         </>
     );
 }
--- frontend/src/components/book/styles.js
+++ frontend/src/components/book/styles.js
@@ -6,39 +6,6 @@ const styles = makeStyles({
         height: '28vh'
     },
 
-    modal: {
-        maxWidth: '60%',
-        minHeight: '60%',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        border: 'none'
-    },
-
-    windowModal: {
-        backgroundColor: '#606060',
-        // alignItems: 'center',
-        // display: 'block',
-        borderRadius: '5px',
-        // borderColor: 'black',
-        padding: '18px 18px 15px 18px',
-        // maxHeight: '100%',
-        // maxWidth: '100%',
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        width: 1200,
-        height: 500,
-        bgcolor: 'background.paper',
-        border: '2px solid #000',
-        boxShadow: 24,
-        p: 4,
-    },
-
 });
 
 export default styles;

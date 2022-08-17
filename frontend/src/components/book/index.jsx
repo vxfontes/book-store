186c794b0611420ba7106a1afc3d6ca765531bdf
vxfontes <nessa1vane@icloud.com>
2022-08-16 21:39:22 -0300

    adicionando botões e corrigindo modal

--- frontend/src/components/book/index.jsx
+++ frontend/src/components/book/index.jsx
@@ -3,18 +3,8 @@ import { Container, Button, Typography, Modal, Fade, Box, Backdrop } from '@mui/
 
 // local
 import styles from './styles';
+import ColorButtonRed from "../buttons/Red";
 
-const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 400,
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
-    boxShadow: 24,
-    p: 4,
-};
 
 const Book = ({ book }) => {
 
@@ -33,14 +23,14 @@ const Book = ({ book }) => {
             <img className={styles().thumb} src={book.image} alt={book.title} width="10%" onClick={handleOpen} />
             
             <Modal className={styles().modal}
-                open={open} onClose={() => setopen(false)} closeAfterTransition BackdropComponent={Backdrop} BackdropProps={{ timeout: 750 }}>
+                open={open} onClose={() => setOpen(false)} closeAfterTransition BackdropComponent={Backdrop} BackdropProps={{ timeout: 750 }}>
                 <Fade in={open}>
-                    <div className={styles().windowModal} style={{ textAlign: 'center' }}>
+                    <Box className={styles().windowModal} style={{ textAlign: 'center' }}>
                         <Typography variant="h4" gutterBottom component="div">Aviso</Typography>
-                        <Typography style={{ marginBottom: '15px' }} variant="body1" gutterBottom component="div">Deseja apagar {subject.name}?</Typography>
-                        <ColorButtonRed style={{ marginRight: '15px' }} variant="contained" color="primary" onClick={() => { deletar(); setopen(false) }}>Sim</ColorButtonRed>
-                        <Button variant="contained" color="secondary" onClick={() => { setopen(false) }}>Não</Button>
-                    </div>
+                        <Typography style={{ marginBottom: '15px' }} variant="body1" gutterBottom component="div">Deseja apagar {book.title}?</Typography>
+                        <ColorButtonRed style={{ marginRight: '15px' }} variant="contained" color="primary" onClick={() => { setOpen(false) }}>Sim</ColorButtonRed>
+                        <Button variant="contained" color="secondary" onClick={() => { setOpen(false) }}>Não</Button>
+                    </Box>
                 </Fade>
             </Modal>
         </>
--- frontend/src/components/book/styles.js
+++ frontend/src/components/book/styles.js
@@ -6,6 +6,19 @@ const styles = makeStyles({
         height: '28vh'
     },
 
+    windowModal: {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: 400,
+        bgcolor: "#ffffff",
+        color: '#ffff',
+        border: '2px solid #000',
+        boxShadow: 24,
+        p: 4,
+    }
+
 });
 
 export default styles;
